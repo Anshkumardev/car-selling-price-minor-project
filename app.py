ff501e2fd8e5eb0e10d8327805166daa9f3583d4
Anshkumardev <anshkumardev@gmail.com>
2022-12-23 23:08:22 +0530

    new Deployment

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request,jsonify
+import pickle
+import numpy as np
+import pandas as pd
+import sklearn
+
+pipe = pickle.load(open('SellCarPriceModel.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello world"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    name = request.form.get('name')
+    company = request.form.get('company')
+    year = request.form.get('year')
+    kms_driven = request.form.get('kms_driven')
+    fuel_type = request.form.get('fuel_type')
+
+    result = pipe.predict(pd.DataFrame([[name, company, year, kms_driven, fuel_type]],columns=["name", "company", "year", "kms_driven", "fuel_type"]))
+
+
+    return jsonify({'predicted_price':str(result)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
